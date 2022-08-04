9a37e6e51e68a010c7bdf7d662c89460054fba47
Turgay <tubas2334@gmail.com>
2022-08-04 13:52:47 +0300

    updated

--- JavaProgramming/src/day38_Inheritance/carTask/ParkingLot.java
+++ JavaProgramming/src/day38_Inheritance/carTask/ParkingLot.java
@@ -1,9 +1,14 @@
 package day38_Inheritance.carTask;
 
+
 public class ParkingLot {
     public static void main(String[] args) {
 
         Toyota toyota = new Toyota("Camry",2020,20000,"Gray",123456);
+
+        Tesla tesla = new Tesla("Model S",2022,94900,"Black",1200);
+
+        BMW bmw = new BMW("X%",2021,23000,"White",15600);
 
 
 
