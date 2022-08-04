9afc75d7e217f13410e98065f82dc3c31ce5bd83
Turgay <tubas2334@gmail.com>
2022-08-04 13:50:09 +0300

    updated

--- JavaProgramming/src/day38_Inheritance/carTask/BMW.java
+++ JavaProgramming/src/day38_Inheritance/carTask/BMW.java
@@ -1,4 +1,18 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car{
+
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+
+    public  void breaksDown(){
+
+        System.out.println(brand + " " + model + " is breaking down");
+
+    }
+
+
+
 }
