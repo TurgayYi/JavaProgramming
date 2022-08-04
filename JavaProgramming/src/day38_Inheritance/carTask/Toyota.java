a609ad8570f3804e1c27cc5b9b06f31acd074ad5
Turgay <tubas2334@gmail.com>
2022-08-04 12:29:30 +0300

    updated

---
+++ JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,19 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car{
+
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+
+
+
+
+
+
+
+
+
+
+}
