03c655a000d66a844b1c90e9461681a955f8b0ad
Turgay <tubas2334@gmail.com>
2022-08-05 13:02:36 +0300

    updated

---
+++ JavaProgramming/src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,43 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle extends Shape{
+
+    private double width, length;
+
+    public Rectangle( double width, double length) {
+        super("Rectangle");
+        setWidth(width);
+        setLength(length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width <= 0){
+            System.err.println("Invalid width " + width);
+            System.exit(1);
+        }
+
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length <= 0){
+            System.err.println("Invalid length " + length);
+            System.exit(1);
+        }
+
+
+        this.length = length;
+    }
+
+
+
+
+}
