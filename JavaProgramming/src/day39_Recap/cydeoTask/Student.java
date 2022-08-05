a7ef9cb2abb95eca2bc1b917d754e04715ccafa1
Turgay <tubas2334@gmail.com>
2022-08-05 18:50:22 +0300

    updated

---
+++ JavaProgramming/src/day39_Recap/cydeoTask/Student.java
@@ -0,0 +1,24 @@
+package day39_Recap.cydeoTask;
+
+public class Student {
+
+
+}
+
+/*
+
+    6. Create a sub class of Person named Student
+
+            Extra variables:
+                studentId, fieldOfStudy
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Encapsulate all the fields
+
+            Methods:
+                study()
+
+ */
