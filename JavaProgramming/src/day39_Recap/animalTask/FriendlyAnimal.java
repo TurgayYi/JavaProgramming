9cc1463ab2f691955590f43a7cda3b59da01e3b0
Turgay <tubas2334@gmail.com>
2022-08-06 03:37:21 +0300

    updated

---
+++ JavaProgramming/src/day39_Recap/animalTask/FriendlyAnimal.java
@@ -0,0 +1,79 @@
+package day39_Recap.animalTask;
+
+public class FriendlyAnimal extends Animal {
+
+    private boolean isWild,isFriendly,isPlayable;
+
+
+    public FriendlyAnimal(String name, String breed, char gender, int age, String size, String color, boolean isWild, boolean isFriendly, boolean isPlayable) {
+        super(name, breed, gender, age, size, color);
+        setWild(isWild);
+        setFriendly(isFriendly);
+        setPlayable(isPlayable);
+    }
+
+    public boolean isWild() {
+        return isWild;
+    }
+
+    public void setWild(boolean wild) {
+        isWild = wild;
+    }
+
+    public boolean isFriendly() {
+        return isFriendly;
+    }
+
+    public void setFriendly(boolean friendly) {
+        isFriendly = friendly;
+    }
+
+    public boolean isPlayable() {
+        return isPlayable;
+    }
+
+    public void setPlayable(boolean playable) {
+        isPlayable = playable;
+    }
+
+    public void play(){
+
+        System.out.println(getBreed() + " " + getName() + " is playing");
+    }
+
+    public void pet(){
+
+        System.out.println(getBreed() + " " + getName() + " is petting");
+    }
+
+
+    public String toString() {
+        return "FriendlyAnimal{" +
+                "name='" + getName() + '\'' +
+                ", breed='" + getBreed() + '\'' +
+                ", gender=" + getGender() +
+                ", age=" + getAge() +
+                ", size='" + getSize() + '\'' +
+                ", color='" + getColor() + '\'' +
+                ", isWild=" + isWild +
+                ", isFriendly=" + isFriendly +
+                ", isPlayable=" + isPlayable +
+                '}';
+
+
+    }
+
+
+
+}
+/*
+	2. Create a sub class of Animal named FriendlyAnimal:
+				Variable:
+					isWild
+					isFriendly
+					isPlayable
+
+				Extra methods:
+					play()
+					pet()
+ */
