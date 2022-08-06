f226a7d46feef814497d2dec8ef2b76ead794d86
Turgay <tubas2334@gmail.com>
2022-08-06 03:56:33 +0300

    updated

---
+++ JavaProgramming/src/day39_Recap/animalTask/Lion.java
@@ -0,0 +1,25 @@
+package day39_Recap.animalTask;
+
+public class Lion extends WildAnimal{
+
+    public Lion(String name, String breed, char gender, int age, String size, String color, boolean isWild, boolean isFriendly, boolean isPlayable) {
+        super(name, breed, gender, age, size, color, isWild, isFriendly, isPlayable);
+    }
+
+
+
+
+
+
+
+
+}
+/*
+	5. Create the following sub classes of WildAnimal and Override the hunt method, and add any extra methods that are needed:
+				1. Lion
+				2. Tiger
+				3. Eagle
+				4. Bear
+				5. Python
+				6. Crocodile
+ */
