527f1b8a1722e0769508abbeb51b82d10ffc877f
Turgay <tubas2334@gmail.com>
2022-08-07 14:43:22 +0300

    updated

---
+++ JavaProgramming/src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,20 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo";
+    protected static String name2 = "WoodenSpoon";
+
+
+    static void method(){
+
+    }
+
+
+    protected static void method2(){
+
+    }
+
+
+
+}
