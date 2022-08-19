c0b93148139270762d45029d88b1d22d1fa08462
Turgay <tubas2334@gmail.com>
2022-08-20 01:08:51 +0300

    updated

--- JavaProgramming/src/day43_Abstraction/employee/Employee.java
+++ JavaProgramming/src/day43_Abstraction/employee/Employee.java
@@ -1,13 +1,16 @@
 package day43_Abstraction.employee;
 
-public abstract class employee extends Person{
+public abstract class Employee extends Person{
 
     private  final int id;
     private String jobTitle;
     private double salary;
 
-    public employee(String name, int age, char gender, int id, String jobTitle, double salary) {
+    public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender);
+        if(id<=0){
+            throw new RuntimeException("Invalid ID " + id);
+        }
         this.id = id;
         setJobTitle(jobTitle);
         setSalary(salary);
---
+++ JavaProgramming/src/day43_Abstraction/employee/Teacher.java
@@ -0,0 +1,4 @@
+package day43_Abstraction.employee;
+
+public class Teacher {
+}
