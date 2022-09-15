1b6c606475e6a3d337fb9cadaccdf891c47b5c43
CYDEO <tubas2334@gmail.com>
2022-09-15 03:22:56 +0300

    created

--- JavaProgramming/src/day47_Polymorphism/ReferenceTypeCasting.java
+++ JavaProgramming/src/day47_Polymorphism/ReferenceTypeCasting.java
@@ -1,11 +1,19 @@
 package day47_Polymorphism;
 
+import day43_Abstraction.employee.Developer;
+import day43_Abstraction.employee.Employee;
+import day43_Abstraction.employee.Person;
+import day43_Abstraction.employee.Tester;
 import day44_Abstraction.animalTask.Animal;
+import day44_Abstraction.animalTask.Cat;
 import day44_Abstraction.animalTask.Dog;
 import day45_Abstraction.shapeTask.Circle;
 import day45_Abstraction.shapeTask.Shape;
+import day45_Abstraction.shapeTask.Square;
 
 public class ReferenceTypeCasting {
+
+
 
 
     public static void main(String[] args) {
@@ -24,6 +32,43 @@ public class ReferenceTypeCasting {
 
         ((Dog) animal).bark();
         dog.bark();
+
+
+        Shape shape1 = new Square(5);
+
+        System.out.println(((Square) shape1).getSide());
+
+
+        System.out.println("-------------------------------");
+
+
+        Animal animal2 = new Cat("Jim","British",'M',3,"Small","White");
+
+       //Cat cat = (Cat) animal2;
+        //cat.meow();
+
+        ((Cat) animal2).meow();
+
+        // ( (Dog) animal2).bark();// it will give ClassCastException
+
+        Employee employee = new Tester("Sherali", 32, 'M',1, "QA", 110000);
+
+        ((Tester) employee).bugReport();
+
+        //((Tester) employee).unitTest();
+        ((Developer) employee).unitTest();//it will give ClassCastException
+
+        //Driver driver = (Driver) employee;
+        Person person = employee;
+        //Teacher teacher = (Teacher) employee;
+
+        System.out.println("-------------------------------");
+
+        Shape s1 = new Circle(10);
+        s1.area();
+
+        //((Cube)s1).volume();
+
 
 
 
