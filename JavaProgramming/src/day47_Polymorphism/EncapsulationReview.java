c06a38609734b9eae366acb78710e3ad763dbba8
CYDEO <tubas2334@gmail.com>
2022-09-15 03:42:51 +0300

    created

---
+++ JavaProgramming/src/day47_Polymorphism/EncapsulationReview.java
@@ -0,0 +1,33 @@
+package day47_Polymorphism;
+
+public class EncapsulationReview {
+
+    private String bookTitle;
+    private final String publishedDate;
+
+    public EncapsulationReview(String bookTitle, String publishedDate) {
+        setBookTitle(bookTitle);
+        this.publishedDate = publishedDate;
+    }
+
+
+    public String getBookTitle() {
+        if (bookTitle==null){
+            return "";
+        }
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        if (bookTitle==null || bookTitle.isEmpty()){
+            throw new RuntimeException("Invalid data");
+        }
+        this.bookTitle = bookTitle;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+
+}
